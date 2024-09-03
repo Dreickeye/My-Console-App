7f3204c1b6d592c9f0b2776284a855daf7437276
Andrei Maxwell <maxwell.473@buckeyemail.osu.edu>
2024-09-03 19:19:03 -0400

    add gitignore

--- obj/Debug/net8.0/My Console App.AssemblyInfo.cs
+++ obj/Debug/net8.0/My Console App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("My Console App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+165932a97a371e3c6895acc4d516de6a4a0215d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fd4b302fbf58bb49bc3835318f6091fd7d09cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("My Console App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("My Console App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
